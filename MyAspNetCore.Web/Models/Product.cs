b50123f93eb6a93eee6af750a98468c96028286c
Gazi Hataş <gazihatas71@gmail.com>
2022-11-05 03:40:59 +0300

    Form Elements Textarea

--- MyAspNetCore.Web/Models/Product.cs
+++ MyAspNetCore.Web/Models/Product.cs
@@ -6,6 +6,7 @@
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+        public string Description { get; set; }
 
         //string bir referans tiptir ve nullable alabilir.
         //.net 6 dan önce olsaydı bu ozellik  ?  eklemeden çalışacaktı
