3967d79fcb3b20d26e9ef70a64338249dd52094a
Gazi Hataş <gazihatas71@gmail.com>
2022-11-06 01:28:37 +0300

    Model/Form Validation Required-3

--- MyAspNetCore.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCore.Web/ViewModels/ProductViewModel.cs
@@ -7,17 +7,22 @@ namespace MyAspNetCore.Web.ViewModels
         public int Id { get; set; }
 
         
-        [System.ComponentModel.DataAnnotations.Required( ErrorMessage = "İsim alanı boş olamaz")]
+        [System.ComponentModel.DataAnnotations.Required( ErrorMessage = "İsim alanı boş olamaz.")]
         public string? Name { get; set; }
-        [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Fiyat alanı boş bırakılamaz.")]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Fiyat alanı boş olamaz.")]
         public decimal Price { get; set; }
-        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Stok alanı boş bırakılamaz.")]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Stok alanı boş olamaz.")]
         public int? Stock { get; set; }
-        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Açıklama alanı boş bırakılamaz.")]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Açıklama alanı boş olamaz.")]
         public string? Description { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Renk seçimi boş olamaz.")]
         public string? Color { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Yayınlanma tarihi boş olamaz.")]
         public DateTime? PublishDate { get; set; }
         public bool IsPublish { get; set; }
-        public int Expire { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Yayınlanma süresi boş olamaz.")]
+        public int? Expire { get; set; }
     }
 }
