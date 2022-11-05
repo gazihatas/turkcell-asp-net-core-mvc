e58313a341219f141245c1e7e223fda7748b7e08
Gazi Hataş <gazihatas71@gmail.com>
2022-11-06 02:06:16 +0300

    StringLength

--- MyAspNetCore.Web/ViewModels/ProductViewModel.cs
+++ MyAspNetCore.Web/ViewModels/ProductViewModel.cs
@@ -7,18 +7,20 @@ namespace MyAspNetCore.Web.ViewModels
     {
         public int Id { get; set; }
 
-        
+        [StringLength(50,ErrorMessage = "İsim alanına en fazla 50 karakter girilebilir.")]
         [System.ComponentModel.DataAnnotations.Required( ErrorMessage = "İsim alanı boş olamaz.")]
         public string? Name { get; set; }
 
-        [Range(1, 200, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır.")]
+        
         [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Fiyat alanı boş olamaz.")]
+        [Range(1, 200, ErrorMessage = "Fiyat alanı 1 ile 1000 arasında bir değer olmalıdır.")]
         public decimal Price { get; set; }
         
         [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Stok alanı boş olamaz.")]
         [Range(1,200, ErrorMessage = "Stok alanı 1 ile 200 arasında bir değer olmalıdır.")]
         public int? Stock { get; set; }
-        
+        [StringLength(300,MinimumLength = 50,ErrorMessage = "Açıklama alanı 50 ile 300 karakter arasında olabilir.")]
+
         [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Açıklama alanı boş olamaz.")]
         public string? Description { get; set; }
         
