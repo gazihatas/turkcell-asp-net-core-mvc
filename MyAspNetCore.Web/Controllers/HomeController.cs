d32ae644aa5a750553c2dd793524705aa768b351
Gazi Hataş <gazihatas71@gmail.com>
2022-11-09 16:22:37 +0300

    Partiak View Model Binding 3. Aşama

--- MyAspNetCore.Web/Controllers/HomeController.cs
+++ MyAspNetCore.Web/Controllers/HomeController.cs
@@ -1,25 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyAspNetCore.Web.Models;
 using System.Diagnostics;
+using MyAspNetCore.Web.ViewModels;
 
 namespace MyAspNetCore.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x =>
+                new ProductPartialViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock
+                }).ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
             return View();
         }
 
         public IActionResult Privacy()
         {
+            var products = _context.Products.OrderByDescending(x => x.Id).Select(x =>
+                new ProductPartialViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    Stock = x.Stock
+                }).ToList();
+
+            ViewBag.productListPartialViewModel = new ProductListPartialViewModel()
+            {
+                Products = products
+            };
+
             return View();
         }
 
