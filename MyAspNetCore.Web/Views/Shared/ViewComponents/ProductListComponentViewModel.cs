33ca2571352e9828969154daedd443d6d9cdb34e
Gazi Hataş <gazihatas71@gmail.com>
2022-11-09 17:16:54 +0300

    View Compoenet Tag Helper

---
+++ MyAspNetCore.Web/Views/Shared/ViewComponents/ProductListComponentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAspNetCore.Web.Views.Shared.ViewComponents
+{
+    public class ProductListComponentViewModel
+    {
+        public string  Name { get; set; }
+        public string  Description { get; set; }
+    }
+}
