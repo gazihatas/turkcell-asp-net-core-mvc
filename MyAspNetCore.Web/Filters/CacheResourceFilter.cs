646c76a7f0d013d688ad73aac16a54e05b68efc6
Gazi Hataş <gazihatas71@gmail.com>
2022-11-14 00:15:44 +0300

    Resource Filter

---
+++ MyAspNetCore.Web/Filters/CacheResourceFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MyAspNetCore.Web.Filters
+{
+    public class CacheResourceFilter:Attribute,IResourceFilter
+    {
+        private static IActionResult _cache;
+
+        //Request girdiği anda
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            _cache = context.Result;
+        }
+
+        //response üretilince çalışan metot
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            if (_cache!=null)
+            {
+                context.Result = _cache;
+            }
+        }
+    }
+}
