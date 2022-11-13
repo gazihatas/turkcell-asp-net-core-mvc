304e8687e29ca60512b47642f239e9b616309c64
Gazi Hataş <gazihatas71@gmail.com>
2022-11-13 23:46:58 +0300

    Resukt Filter

--- MyAspNetCore.Web/Controllers/OrnekController1.cs
+++ MyAspNetCore.Web/Controllers/OrnekController1.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCore.Web.Filters;
 
 namespace MyAspNetCore.Web.Controllers
 {
+    public class Product2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    [CustomResultFilter("x-version","1.0")]
+    [Route("[controller]/[action]")]
     public class OrnekController1 : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            var productList = new List<Product2>()
+            {
+                new(){Id=1, Name="Kalem"},
+                new(){Id=2, Name = "Dester"},
+                new(){Id=3,Name = "Silgi"}
+            };
+            return View(productList);
         }
     }
 }
