a1a0b9a68a0d17abddd8ab7e89f7e978d15d4bdd
Gazi Hatas <gazihatas71@gmail.com>
2022-11-15 05:25:09 +0300

    Appsettings.json

---
+++ MyAspNetCore.Web/Controllers/AppSettingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCore.Web.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AppSettingController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.baseUrl = _configuration["baseUrl"];
+            ViewBag.smsKey = _configuration["Keys:Sms"];
+            ViewBag.emailKey = _configuration.GetSection("Keys")["email"];
+            return View();
+        }
+    }
+}
