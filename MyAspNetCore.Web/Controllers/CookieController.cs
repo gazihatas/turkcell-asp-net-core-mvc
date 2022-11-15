5ec4e3edf2fcdaa6769950b0cc82366525000e03
Gazi Hatas <gazihatas71@gmail.com>
2022-11-15 05:50:46 +0300

    Cookie

---
+++ MyAspNetCore.Web/Controllers/CookieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCore.Web.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CookieController : Controller
+    {
+        public IActionResult CookieCreate()
+        {
+            HttpContext.Response.Cookies.Append("background-color", "red", new CookieOptions()
+            {
+                Expires=DateTime.Now.AddDays(2)
+            });
+            return View();
+        }
+
+        public IActionResult CookieRead()
+        {
+            var bgcolor = HttpContext.Request.Cookies["background-color"];
+
+            ViewBag.bgcolor = bgcolor;
+            return View();
+        }
+    }
+}
